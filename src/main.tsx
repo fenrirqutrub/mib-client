73b834b5c3ff27b9f6d09a36fd58bfb56d53b66c
fenrirqutrub <fenrirqutrub@gmail.com>
2026-02-22 18:43:32 +0600

    update something

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import Router from "./Router/Router.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "./context/ThemeProvider.tsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -25,6 +26,7 @@ createRoot(document.getElementById("root")!).render(
         <ThemeProvider>
           <Router />
           <Toaster position="top-right" />
+          <SpeedInsights />
         </ThemeProvider>
       </QueryClientProvider>
     </BrowserRouter>
